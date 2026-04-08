c8cc3bd69b9663c7c043453f098119253d72bd80
Juannito Eryadi <JJuannitoEryadi@gmail.com>
2026-04-08 17:52:01 +0700

    perbaikan prefix

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // WAJIB: Tambahkan ini agar NestJS tahu dia jalan di path /API milik cPanel
-  app.setGlobalPrefix('API');
+  // app.setGlobalPrefix('API');
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
